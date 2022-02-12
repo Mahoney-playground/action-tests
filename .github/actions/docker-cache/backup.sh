75128d2422d087df73d688ddbbcb2ec95c10ff5b
Robert Elliot <rob@lidalia.org.uk>
2022-02-12 12:32:00 +0000

    Eh?

--- .github/actions/docker-cache/backup.sh
+++ .github/actions/docker-cache/backup.sh
@@ -7,6 +7,8 @@ set -exuo pipefail
 main() {
   local cache_dir=$1
 
+  whoami
+
   rm -rf "$cache_dir"
   mkdir -p "$(dirname "$cache_dir")"
 
@@ -14,6 +16,7 @@ main() {
   timing sudo mv /var/lib/docker "$cache_dir"
   timing sudo chown -R "$USER:$(id -g -n "$USER")" "$cache_dir"
   sudo ls -lh "$cache_dir"
+  sudo ls -lh "$cache_dir/overlay2/"
   sudo du -sh "$cache_dir"
 }
 
