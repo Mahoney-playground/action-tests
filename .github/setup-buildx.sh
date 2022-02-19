e30793db3ea4c39804ece83b5d12b752685ceef2
Robert Elliot <rob@lidalia.org.uk>
2022-02-19 20:28:17 +0000

    Make sure the builder is running

--- .github/setup-buildx.sh
+++ .github/setup-buildx.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 docker run --rm --privileged tonistiigi/binfmt:qemu-v6.1.0-21 --install all
 
 if ! docker buildx inspect builder > /dev/null 2>&1; then
-  docker buildx create --use --name builder
+  docker buildx create --driver docker-container --use --bootstrap --name builder
   docker buildx install
 fi
