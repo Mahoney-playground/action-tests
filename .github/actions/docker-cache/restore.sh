c3a09f99c56e44214176519b12215cca09c35c10
Robert Elliot <rob@lidalia.org.uk>
2022-02-19 20:33:01 +0000

    And again

--- .github/actions/docker-cache/restore.sh
+++ .github/actions/docker-cache/restore.sh
@@ -7,12 +7,13 @@ set -euo pipefail
 main() {
   local full_path_to_cache_tar=$1
   local cache_tar; cache_tar=$(basename "$full_path_to_cache_tar")
+  local cache_dir; cache_dir=$(dirname "$full_path_to_cache_tar")
 
   if [[ -f "$full_path_to_cache_tar" ]]; then
     docker run --rm \
       --volumes-from buildx_buildkit_builder0 \
-      -v "$(pwd)":/backup \
-      alpine \
+      -v "$cache_dir":/backup \
+      alpine:3.15.0 \
       sh -c "cd /var/lib/buildkit && tar xvf /backup/$cache_tar --strip 1"
   fi
 }
